4c96d546647122a07fffeffc95e44aef3808b897
adriantech-beep <Adhrianne29@gmail.com>
2024-09-15 17:11:01 +0800

    updated all files and added new files

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 
 const appIconShare = document.querySelector(".app-icon-share");
 const modalIcons = document.querySelector(".modal-icons");
-const overlay = document.querySelector(".overlay");
 const appGroupTwo = document.querySelector(".app-group-two");
 const appIconShareMobile = document.querySelector(".app-icon-share-mobile");
 const appGroupFour = document.querySelector(".app-group-four");
 const mobileModal = document.querySelector(".mobile-modal");
+const appCloseIcon = document.querySelector(".app-close-icon");
 
 const remove = function () {
   modalIcons.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  appCloseIcon.classList.remove("hidden");
 };
-const add = function () {
-  modalIcons.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+
 appIconShare.addEventListener("click", remove);
-overlay.addEventListener("click", add);
+appCloseIcon.addEventListener("click", function () {
+  modalIcons.classList.add("hidden");
+  appCloseIcon.classList.add("hidden");
+});
 
 appIconShareMobile.addEventListener("click", function () {
   appGroupTwo.classList.add("hidden");
@@ -48,8 +48,8 @@ appIconShareMobile.addEventListener("click", function () {
                   ></a>
                 </div>
               </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg" class="mobile-share-button"
+              <svg class="app-share-btn"
+                xmlns="http://www.w3.org/2000/svg"
                 width="32"
                 height="32"
                 viewBox="0 0 32 32"
